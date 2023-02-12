b9087946a3c7c52bec1222282a35d0446230ddc7
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2023-02-12 11:09:57 +0330

    chore: update 2.15 sha256

--- Casks/nekoray.rb
+++ Casks/nekoray.rb
@@ -1,6 +1,6 @@
 cask "nekoray" do
     version "2.15"
-    sha256 "40a9b22c9f225b2ea663a07d0af31002cb87599edab4e631a10e0e77330ee9d8"
+    sha256 "e3ec443d8c531054177d1b698210d8d64d7fa5e66df754d438c088033123d739"
   
     url "https://github.com/tdjnodj/nekoray/releases/download/#{version}/nekoray-2.15-2023-02-12-macos-amd64.dmg",
         verified: "github.com/MatsuriDayo/nekoray/"
