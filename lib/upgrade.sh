11dac646f40b70bd67ba83647c6ce422e311a5fb
tdjnodj <108401163+tdjnodj@users.noreply.github.com>
2023-02-12 16:00:37 +0800

    chore: remove tmp

--- lib/upgrade.sh
+++ lib/upgrade.sh
@@ -20,12 +20,13 @@ newVersion=$(curl https://raw.githubusercontent.com/MatsuriDayo/nekoray/main/nek
 
 oldSha256=$(cat Casksnekoray.rb | grep "sha256" | cut -d '"' -f 2)
 
-mkdir tmp/
-cd tmp/
+mkdir tmp_dmg/
+cd tmp_dmg/
 curl -L -O https://github.com/tdjnodj/nekoray/releases/download/${newTag}/nekoray-${newVersion}-macos-amd64.dmg
 newSha256=$(opensll dgst -sha256 nekoray-${newVersion}-macos-amd64.dmg)
 
 cd ..
+rm -rf tmp_dmg/
 
 # Change the file.
 sed -i "s/${oldSha256}/${newSha256}/g" Casks/nekoray.rb
